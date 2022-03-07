779e8c77c30190126f3610b935ab90b3875df2ad
EC2 Default User <ec2-user@ip-172-31-1-180.us-west-2.compute.internal>
2022-03-07 00:02:06 +0000

    server

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask,render_template,request
+import numpy as np
+from deepface import DeepFace
+from deepface.commons import functions
+import cv2
+import base64
+app = Flask('__name__')
+count = 0
+happy_level = 0
+
+emo_model = DeepFace.build_model('Emotion')
+emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+def emo(img_path , detector_backend = 'opencv'):
+
+        resp_obj = {}
+
+        try:
+            img, region = functions.preprocess_face(img = img_path, target_size = (48, 48), grayscale = True, enforce_detection = True, detector_backend = detector_backend, return_region = True)
+        except:
+            return {}
+        emotion_predictions = emo_model.predict(img)[0,:]
+        sum_of_predictions = emotion_predictions.sum()
+
+        for i in range(0, len(emotion_labels)):
+            emotion_label = emotion_labels[i]
+            emotion_prediction = 100 * emotion_predictions[i] / sum_of_predictions
+            resp_obj[emotion_label] = emotion_prediction
+
+        resp_obj["dominant_emotion"] = emotion_labels[np.argmax(emotion_predictions)]
+        return resp_obj
+
+@app.route('/')
+def home():
+    print("RECEIVE")
+    return "HOME"
+
+@app.route('/reset')
+def reset():
+    global count, happy_level
+    c = count
+    count = 0
+    happy_level = 0
+    return f"{happy_level}"
+
+@app.route('/image', methods=['POST'])
+def image():
+    global count,happy_level
+    data = request.json
+    #h,w = data['height'], data['width']
+    #img = np.fromstring(data['image'],dtype=np.uint8).reshape(h,w,-1)
+    img = base64_bytes = data['image'].encode('utf-8')
+    img = base64.b64decode(img)
+    img = np.fromstring(img,dtype=np.uint8)
+
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    emotion = emo(img)
+    print(emotion)
+    if emotion:
+        happy_level += float(emotion['happy'])
+    count += 1
+    if count == 0:
+        return 0
+    return f'{happy_level/count}'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
+    #app.run(debug=True) #can alter host and port number here. Right now the default host is localhost and port is 5000
+
