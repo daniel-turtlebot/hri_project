cd3f1e381d29d6581706807ac75c6a42c1f56ae9
daniel-turtlebot <dbeaglehole@Daniels-MacBook-Pro-2.local>
2022-03-10 20:53:21 -0800

    updated run

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
+import os
+import numpy as np
 from src import learning_utils 
-
-class Action:
-    
-
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
 class GameBot:
     def __init__(self):
@@ -15,11 +15,20 @@ class GameBot:
         self.sampler = learning_utils.Sampler(self.num_actions)
 
         self.testing = True
+        self.t = 0
+        self.X = [] 
+        self.y = [] 
 
     def do_action(self,action_idx):
         """
         Executes the action indexed by the argument. 
         Returns the reward from that action (perceived enjoyment)
+        """
+        pass
+    
+    def action_to_x(self,a_t):
+        """ 
+        Convert action index to a single vector of categorical vars
         """
         pass
 
@@ -28,14 +37,19 @@ class GameBot:
         Run experiments
         """
 
-        # sample an action
+        ## sample an action
         a_t = sampler.sample()
 
-        # perform the action
-        reward = do_action(a_t)
+        ## perform the action
+        y_t = do_action(a_t) # return reward
 
-        # update sampling table
-        sampler.update_index(reward,a_t)
+        ## update sampling table
+        sampler.update_index(y_t,a_t)
+
+        x_t = self.action_to_x(a_t)
+        self.t += 1
+        self.X.append(x_t)
+        self.y.append(y_t)
          
 
 if __name__ == "main":
@@ -43,4 +57,21 @@ if __name__ == "main":
 
     # If given a start signal 
     gamebot.run()
+
+    Xs = gamebot.X
+    ys = gamebot.y
+
+    model = LogisticRegression(solver='liblinear', random_state=0)
+    model.fit(Xs, ys)
+
+    cwd = os.getcwd()
+    data_file = cwd + "/estimator.txt"
+    params_file = cwd + "/params.txt"
+
+    np.savetxt(data_file,Xs)
+    np.savetxt(data_file,ys)
+    np.savetxt(params_file,model.intercept_)
+    np.savetxt(params_file,model.coef_)
+
+
 
